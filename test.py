fd39bea648d4732c94decd34b3f73f616790ff94
F. Bouvier d'Yvoire <722_francois@live.fr>
2017-11-04 18:46:37 +0100

    Config file operationnel

--- test.py
+++ test.py
@@ -4,56 +4,47 @@ from fonction import Sigmoid, MnistTest, Norm2
 from mnist import MNIST
 from engine import Engine
 from dataInterface import DataInterface
+from configLoader import ConfigLoader
 
+cfg = ConfigLoader('config.ini')
+param = cfg.read()
 
-mndata = MNIST('./data')
+mndata = MNIST(param['file'])
 training_images, training_labels = mndata.load_training()
 testing_images, testing_labels = mndata.load_testing()
 
 training_images = np.array(training_images)
 training_labels = np.array(training_labels)
-training_size = 1000
+training_size = param['training_size']
 
 testing_images = np.array(testing_images)
 testing_labels = np.array(testing_labels)
-testing_size = 10
+testing_size = param['testing_size']
 
-learning_iterations = 1
-test_period = 100
-randomize_learning_set = True
-
-
-activation_funs = np.array([Sigmoid(0.1), Sigmoid(0.1), Sigmoid(0.1)])
-error_fun = Norm2()
-
-net = Network([784, 10, 10, 10], activation_funs, error_fun)
-
-eta = 0.2
+learning_iterations = param['learning_iterations']
+test_period = param['test_period']
+randomize_learning_set = param['learning_iterations']
 
 
-# def training_fun(n):
-#     r = np.zeros(10)
-#     r[training_labels[n]] = 1
-#     r = np.reshape(r, (10,1))
-#     return r
+activation_funs = np.array(param['activation_funs'])
+error_fun = param['error_fun']
+
+net = Network(param['network_layers'], activation_funs, error_fun)
+
+eta = param['eta']
 
 
-# def testing_fun(n):
-#     r = np.zeros(10)
-#     r[testing_labels[n]] = 1
-#     r = np.reshape(r, (10,1))
-#     return r
-training_fun = MnistTest(training_labels)
-testing_fun = MnistTest(testing_labels)
+training_fun = param['training_fun'](training_labels)
+testing_fun = param['testing_fun'](testing_labels)
 
 
-engine = Engine(net, eta, training_images[0:1000] / 256, training_fun, testing_images[0:1000] / 256,
-                testing_fun, 1, 100)
+engine = Engine(net, eta, training_images[0:training_size] / 256, training_fun, testing_images[0:testing_size] / 256,
+                testing_fun, learning_iterations, test_period, randomize_learning_set)
 
 error_during_learning = engine.run()
 
 data_interface = DataInterface('Mnist_debug')
 
 data_params = np.array([learning_iterations, test_period, training_size, testing_size, eta])
-param_description = '[learning_iterations, test_period, training_size, testing_size, eta] + Network([784, 10, 10, 10], [Sigmoid(0.1), Sigmoid(0.1), Sigmoid(0.1)]'
+param_description = str(param)
 data_interface.save(error_during_learning, 'error_during_learning', data_params, param_description)
