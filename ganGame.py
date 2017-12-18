affc1f04d75b31a4bb8acaa6cfc78fb307bba28b
FrouxBY <francois.dyvoire@larez.fr>
2017-12-18 19:18:13 +0100

    Complement du GanGame (sauf géération de bruit)

--- ganGame.py
+++ ganGame.py
@@ -9,6 +9,14 @@ class GanGame:
     Class of en GAN game, i.e two network learning together with the GAN theorie
     """
 
+    ##
+    ## @brief      Constructs the object.
+    ##
+    ## @param      self            The object
+    ## @param      discriminator   The discriminator (will be a Engine object)
+    ## @param      generator       The generator (will be a only a network)
+    ## @param      learning_ratio  The learning ratio between discrimator and generator
+    ##
     def __init__(self, discriminator, generator, learning_ratio=1):
         self.generator = generator
         self.discriminator = discriminator
@@ -16,21 +24,30 @@ class GanGame:
 
     def playAndLearn(self):
         for i in range(learning_ratio):
-            self.discriminatorLearning()
+            self.discriminatorLearningPositive()
         noise = self.generateNoise()
         fake_image = self.generateImage(noise)
 
         fooled = testTruth(fake_image)
 
-        generatorLearning(fooled)
+        generatorLearning(fooled, noise)
+        discriminatorLearningNegative(fake_image)
         return fooled
 
-    def discriminatorLearning():
+    def discriminatorLearningPositive():
         self.discriminator.learn()   
         pass
 
-    def generatorLearning():
-        pass
+    def discriminatorLearningNegative(fake_image, noise):
+        self.discriminator.net.backprop(self.discriminator.eta, fake_image, noise)
+    ##
+    ## @brief      initiate backprop for generator
+    ##
+    ## @param      fooled  result between 0 and 1 if G fooled D or not
+    ##
+    ## @comment    The cost function will be initialize with the network.
+    def generatorLearning(fooled, noise):
+        self.generator.backprop(eta, noise, fooled)
 
     ##
     # @brief      { function_description }
@@ -43,7 +60,7 @@ class GanGame:
         Image = generator.net.compute(noise)
         return Image
 
-    def generateNoise():
+    def generateNoise():  # pas forcément adapté de l'avoir ici
         random = np.random()
         self.noiseFunction.out()
 
