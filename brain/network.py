correct inputs reshape for backprop

brain/network.py
@@ -90,7 +90,14 @@ class Network:
     #
 
     def backprop(self, eta, inputs, reference):
-        inputs = np.reshape(inputs, (len(inputs), 1))
+        dim = np.shape(inputs)
+        nb_dim = len(dim)
+        if nb_dim == 1:  # Pour conserver le fonctionnement avec un vecteur simple en entrée
+            inputs = np.reshape(inputs, (dim[0], 1))
+        elif nb_dim == 2:
+            inputs = np.reshape(inputs, (dim[1], dim[0]))
+        else:
+            raise Exception("Incorrect inputs dimensions")
         n = self._layers_count
         # Si l'entrée et la sortie sont la même couche
         if n == 1: